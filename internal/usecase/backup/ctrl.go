f88362b0ba45380d890e9f9c0e6074c42a236061
Saveliev Anton <saveliev.ant@gmail.com>
2023-08-10 14:58:42 +0300

    init

---
+++ internal/usecase/backup/ctrl.go
@@ -0,0 +1,50 @@
+package backup
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/antonmisa/1cctl_cli/internal/entity"
+)
+
+// CtrlBackup -.
+type CtrlBackup struct {
+	pathTo1C string
+}
+
+// New -.
+func New(path string) (*CtrlBackup, error) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return nil, fmt.Errorf("1cv8 executable file does not exist: %w", err)
+	}
+
+	ctrl := &CtrlBackup{
+		pathTo1C: path,
+	}
+
+	return ctrl, nil
+}
+
+// RunBackup -.
+func (r *CtrlBackup) RunBackup(ctx context.Context,
+	cl entity.Cluster, ib entity.Infobase,
+	ibCred entity.Credentials,
+	lockCode string,
+	outputPath string) error {
+
+	cmd := exec.CommandContext(ctx, r.pathTo1C, "CONFIG", "/S", fmt.Sprintf("%s:%s\\%s", cl.Host, cl.Port, ib.Name),
+		"/N", ibCred.Name, "/P", ibCred.Pwd,
+		"/UC", lockCode, "/DisableStartupMessages",
+		"/DumpIB", outputPath) //nolint:gosec // it is normal
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("ctrlbackup - runbackup - cmd.Run: %w", err)
+	}
+	defer cmd.Cancel()
+
+	return nil
+}
